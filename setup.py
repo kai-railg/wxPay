75a423ea7bf9fc84c2d0b97e9af9a14e1e25edac
kai <1010547513@qq.com>
2020-12-14 16:45:49 +0800

    add wxpay

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='pywxpay',
+    version='0.4',
+    description='python wxpay sdk.',
+    long_description=open('README.rst').read(),
+    author='wxpay',
+    url='https://github.com/wxpay/WXPay-SDK-Python',
+    author_email='wxpay1888@foxmail.com',
+    license='BSD',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    install_requires=[
+        'requests'
+    ],
+
+    py_modules=['pywxpay', ],
+
+)
